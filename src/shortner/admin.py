d0cc7b5460aaf9e1f2b3441accdda571a8a9c6b6
Kim Dillon <Kim@wwwws-MacBook-Pro.local>
2017-03-03 22:04:11 -0800

    Create first model to add UrlApp to Django Admin.
    Create class in model with a char field.
    Register UrlApp in admin file.

---
+++ src/shortner/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import UrlApp
+
+admin.site.register(UrlApp)
---
+++ src/shortner/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class UrlApp (models.Model):
+    url = models.CharField(max_length=220, )
+
+    def __str__(self):
+        return str(self.url)
