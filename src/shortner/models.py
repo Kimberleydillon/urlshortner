6126815d4f352f543616c131b1279e9d2a7b2709
Kim Dillon <Kim@wwwws-MacBook-Pro.local>
2017-03-07 20:06:41 -0800

    Add shortcode for urls, timestamp, updated time and unique id
    Delete old migrations nad sqlite.
    Create one migration with all model fields and remigrate.

--- src/shortner/models.py
+++ src/shortner/models.py
@@ -4,6 +4,12 @@ from django.db import models
 
 class UrlApp (models.Model):
     url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=15, unique=True)# in database this creates whole new column.
+    updated = models.DateTimeField(auto_now=True) #everytime model is saved time value is set
+    timestamp = models.DateTimeField(auto_now_add=True) #when model was created
 
     def __str__(self):
+        return str(self.url)
+
+    def __unicode__(self):
         return str(self.url)
