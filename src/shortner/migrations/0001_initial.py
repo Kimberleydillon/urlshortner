adbd46b1db95fca76cd93973a01cf69dd0cb4ac4
Kim Dillon <Kim@wwwws-MacBook-Pro.local>
2017-03-09 21:01:13 -0800

    Make short url generator and create utils file for placing functions
    short url generator function in utils and imported into model.
    set short charfield to blank=true.
    overrode save method to practice overriding methods and to make sure code generator function works.

--- src/shortner/migrations/0001_initial.py
+++ src/shortner/migrations/0001_initial.py
@@ -24,3 +24,5 @@ class Migration(migrations.Migration):
             ],
         ),
     ]
+
+
--- src/shortner/models.py
+++ src/shortner/models.py
@@ -2,11 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+from .utils import code_generator
+
+
 class UrlApp (models.Model):
     url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15, unique=True)# in database this creates whole new column.
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)# in database this creates whole new column.
     updated = models.DateTimeField(auto_now=True) #everytime model is saved time value is set
     timestamp = models.DateTimeField(auto_now_add=True) #when model was created
+
+
+    def save(self, *args, **kwargs):
+        print ("something")
+        self.shortcode = code_generator()
+        super(UrlApp, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
