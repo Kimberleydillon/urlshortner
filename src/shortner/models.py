f5a2c79f8d6dd9879476f2c55e288317aa90def8
Kim Dillon <Kim@wwwws-MacBook-Pro.local>
2017-03-12 22:22:28 -0700

    Generate dynamic shortcode
    Create instance of UrLApp class in utils and model built method filter to ensure the same shortcode can not be used twice.
    Refractor save override method to only generate shortcode if field is null or blank.

--- src/shortner/models.py
+++ src/shortner/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-from .utils import code_generator
+from .utils import code_generator,create_shortcode
 
 
 class UrlApp (models.Model):
@@ -12,9 +12,9 @@ class UrlApp (models.Model):
     timestamp = models.DateTimeField(auto_now_add=True) #when model was created
 
 
-    def save(self, *args, **kwargs):
-        print ("something")
-        self.shortcode = code_generator()
+    def save(self, *args, **kwargs): #overrides save method
+        if self.shortcode is None or self.shortcode == "":
+            self.shortcode = create_shortcode(self)
         super(UrlApp, self).save(*args, **kwargs)
 
     def __str__(self):
--- src/shortner/utils.py
+++ src/shortner/utils.py
@@ -3,3 +3,12 @@ import string
 
 def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
     return''.join(random.choice(chars) for _ in range(size))
+
+def create_shortcode(instance, size=6):
+    new_code = codegenerator(size=size)
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(shortcode=new_code).exists()
+    if qs_exists:
+        return create_shortcode(size=size)
+    return new_code
+
