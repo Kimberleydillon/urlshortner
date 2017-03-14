8f7ec46fbf4af367de8b8106c89f7d30b83cae49
Kim Dillon <Kim@wwwws-MacBook-Pro.local>
2017-03-13 20:23:04 -0700

    Write a model manager class and def for refreshing short codes
    Create a model manager to practice creating model methods.
    Write def all to only query active urls.
    Write model method to refresh short codes.

--- src/shortner/models.py
+++ src/shortner/models.py
@@ -2,14 +2,33 @@ from django.db import models
 
 # Create your models here.
 
-from .utils import code_generator,create_shortcode
+from .utils import code_generator, create_shortcode
+
+class UrlAppManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs_main = super(UrlAppManager, self).all(*args, **kwargs)
+        qs = qs_main.filter(active=True)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = UrlApp.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            print(q.shortcode)
+            q.save()
+            new_codes += 1
+        return "New codes made: {i}".format(i=new_codes)
 
 
 class UrlApp (models.Model):
-    url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15, unique=True, blank=True)# in database this creates whole new column.
-    updated = models.DateTimeField(auto_now=True) #everytime model is saved time value is set
-    timestamp = models.DateTimeField(auto_now_add=True) #when model was created
+    url         = models.CharField(max_length=220, )
+    shortcode   = models.CharField(max_length=15, unique=True, blank=True)# in database this creates whole new column.
+    updated     = models.DateTimeField(auto_now=True) #everytime model is saved time value is set
+    timestamp   = models.DateTimeField(auto_now_add=True) #when model was created
+    active      = models.BooleanField(default=True)
+    objects     = UrlAppManager()
+    some_random = UrlAppManager()
 
 
     def save(self, *args, **kwargs): #overrides save method
--- src/shortner/utils.py
+++ src/shortner/utils.py
@@ -5,7 +5,7 @@ def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
     return''.join(random.choice(chars) for _ in range(size))
 
 def create_shortcode(instance, size=6):
-    new_code = codegenerator(size=size)
+    new_code = code_generator(size=size)
     Klass = instance.__class__
     qs_exists = Klass.objects.filter(shortcode=new_code).exists()
     if qs_exists:
